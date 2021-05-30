f17d140fd0ad45d557e5965cedd280e63cf21125
lkmSasanga <lkmsasanga@gmail.com>
2021-05-30 12:24:43 +0530

    delete method added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from fastapi.param_functions import Query
 from pydantic import BaseModel
 from pydantic.types import StrIntFloat
 import uvicorn
@@ -46,24 +47,67 @@ class Blog(BaseModel):
 def create_blog(blog: Blog):
     return {'data': f'Blog is created as {blog.title}'}
 
-inventory = {
-    1: {
-        "name": "Milk",
-        "price": 34.89,
-        "brand": "Highland"
-    }
-}
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
+
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
+
+inventory = {}
 
 @app.get('/get-item/{item_id}')
-def get_item(item_id: int = Path(None, description="This ID of the item you like to view")):
+def get_item(item_id: int = Path(None, description="This ID of the item you like to view", gt=0)):
     return inventory[item_id]
 
 @app.get('/get-by-name')
-def get_item(name: str = None):
+def get_item(name: Optional[str] = Query(None, title="Name", description="Name of item.")): 
     for item_id in inventory:
-        if inventory[item_id]["name"] == name:
+        if inventory[item_id].name == name:
             return inventory[item_id]
     return {"Data": "Not found"}
+
+
+
+
+@app.post('/create-item/{item_id}')
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error": "Item ID already exists."}
+    
+    inventory[item_id] = item
+    return inventory[item_id]
+
+
+
+@app.put('/update-item/{item_id}')
+def update_item(item_id: int, item: UpdateItem):
+    if item_id not in inventory:
+        return {"Error": "Item ID does not exists"}
+
+    if item.name != None:
+        inventory[item_id].name = item.name
+
+    if item.price != None:
+        inventory[item_id].price = item.price
+
+    if item.brand != None:
+        inventory[item_id].brand = item.brand
+
+    return inventory[item_id]
+
+
+@app.delete("/delete-item")
+def delete_item(item_id: int = Query(..., description="The ID of the item to delete", gt=0)):
+    if item_id not in inventory:
+        return {"Error": "ID does not exists."}
+
+    del inventory[item_id]
+    return {"Success": "Item deleted"}
 
 
 if __name__ == "__main__":
